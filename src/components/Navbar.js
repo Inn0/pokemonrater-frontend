ba4977fdf7eab88025d93d185ed1078b5f3077ec
Inn0 <daan.brocatus@outlook.com>
2021-06-10 14:34:15 +0200

    Add icons to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import { VALUES } from '../constants/values'
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+import { FaSearch, FaStar } from 'react-icons/fa'
 
 const NavbarContainer = styled.div`
     height: ${VALUES.navbarHeight};
@@ -20,13 +22,49 @@ const StyledLink = styled(Link)`
     text-decoration: none;
     color: black;
     margin: auto;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    text-align: center;
+`
+
+const NavTitle = styled.p`
+    font-size: 16px;
+    margin: 0;
+    line-height: calc(${VALUES.navbarHeight} / 2);
+    margin-top: -5px;
+    font-weight: 550;
+`
+
+const SearchIcon = styled(FaSearch)`
+    height: calc((${VALUES.navbarHeight} / 2) - 5px);
+    width: calc((${VALUES.navbarHeight} / 2) - 5px);
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 5px;
+`
+
+const FaveIcon = styled(FaStar)`
+    height: calc((${VALUES.navbarHeight} / 2) - 5px);
+    width: calc((${VALUES.navbarHeight} / 2) - 5px);
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 5px;
 `
 
 function Navbar() {
+    const location = useLocation()
+
     return (
         <NavbarContainer>
-            <StyledLink to="/">Search</StyledLink>
-            <StyledLink to="/favorites">Favorites</StyledLink>
+            <StyledLink to="/">
+                <SearchIcon style={{ color: location.pathname === '/' ? "red" : "black" }}></SearchIcon>
+                <NavTitle style={{ color: location.pathname === '/' ? "red" : "black" }}>Search</NavTitle>
+            </StyledLink>
+            <StyledLink to="/favorites">
+                <FaveIcon style={{ color: location.pathname === '/favorites' ? "red" : "black" }}></FaveIcon>
+                <NavTitle style={{ color: location.pathname === '/favorites' ? "red" : "black" }}>Search</NavTitle>
+            </StyledLink>
         </NavbarContainer>
     )
 }
